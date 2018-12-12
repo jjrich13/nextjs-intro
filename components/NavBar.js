b7a351952136104f6e16c2ff3380631abada3592
Jeff Richardson <jjrich13@gmail.com>
2018-12-12 14:43:26 -0600

    Initial commit

---
+++ components/NavBar.js
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+const NavBar = () => (
+    <div>
+        <ul>
+            <li>
+                <Link href='/'><a>Home</a></Link>
+            </li>
+            <li>
+                <Link href='/about'><a>About</a></Link>
+            </li>
+        </ul>
+        {/* <style jsx></style> */}
+    </div>
+)
+
+export default NavBar;
